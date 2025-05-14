f772a55b2a0157a878ce309bde4d84f0d44849a8
Yonas <yonas.y@gmail.com>
2025-05-14 12:48:18 -0400

    feat: Use color backtracing.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,16 @@
+#![feature(try_blocks)]
+
 use chat_rooms::app::App;
 use loco_rs::cli;
 use migration::Migrator;
+use color_eyre::eyre::{Result, ErrReport};
 
 #[tokio::main]
-async fn main() -> loco_rs::Result<()> {
-    cli::main::<App, Migrator>().await
+async fn main() -> Result<(), ErrReport> {
+    color_eyre::install()?;
+
+    match cli::main::<App, Migrator>().await {
+        Ok(_) => { Ok(()) }
+        Err(e) => { Err(ErrReport::new(e)) }
+    }
 }
